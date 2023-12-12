ecf42172ca6d268111e660cdbbbc7ef1c1546cde
Vinicius Motta <viniciusmotta8@gmail.com>
2023-12-11 23:51:21 -0300

    feat: backend url added

--- utils/get-info.tsx
+++ utils/get-info.tsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const getInfo = async (token: string) => {
-  const response = await axios.get(`${process.env.AUTHORIZATION_URL}/info`, {
+  const response = await axios.get(`${process.env.BACKEND_URL}/info`, {
     headers: {
       authorization: `Bearer ${token}`
     }
   })
 
   const info = {
-    authUrl: process.env.AUTHORIZATION_URL,
+    authUrl: process.env.BACKEND_URL,
     planType: response.data.plan_type,
     planLimit: response.data.plan_limit,
     planUsed: response.data.plan_used,
